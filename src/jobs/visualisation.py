726cc318b9d4fcbea602a0ce5a5220cb610d7b2b
madhav_sidd <madhavsid99@gmail.com>
2024-09-10 15:01:13 +0530

    Updated visualisation logic for visa data processi

--- src/jobs/visualisation.py
+++ src/jobs/visualisation.py
@@ -1,26 +1,26 @@
 from pyspark.sql import SparkSession
 
 
-spark = SparkSession.builder.appName('Processing').getOrCreate()
+spark = SparkSession.builder.appName('Processing').master('spark://172.24.0.2:7077').getOrCreate()
 print("SparkSession created successfully.")
 
 
 #df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("inputs/visa_number_in_japan.csv")
-#df = spark.read.csv('/opt/bitnami/spark/input/*', header=True, inferSchema=True)
-data = [("Alice", 1), ("Bob", 2), ("Cathy", 3)]
-df = spark.createDataFrame(data, ["Name", "Id"])
+df = spark.read.csv('/opt/bitnami/spark/input/*', header=True, inferSchema=True)
+#data = [("Alice", 1), ("Bob", 2), ("Cathy", 3)]
+#df = spark.createDataFrame(data, ["Name", "Id"])
 
 # standardize or clean the columns
-#new_column_names = [col_name.replace(' ', '_')
-#                   .replace('/', '')
- #                  .replace('.', '')
-  #                  .replace(',', '')
-   #                 for col_name in df.columns]
-#df = df.toDF(*new_column_names)
+new_column_names = [col_name.replace(' ', '_')
+                    .replace('/', '')
+                    .replace('.', '')
+                    .replace(',', '')
+                    for col_name in df.columns]
+df = df.toDF(*new_column_names)
 
-#df= df.dropna(how='all')
+df= df.dropna(how='all')
 
-#df = df.select('year', 'country', 'number_of_issued_numerical')
+df = df.select('year', 'country', 'number_of_issued_numerical')
 
 df.show()
 
